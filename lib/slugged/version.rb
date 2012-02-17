75c8e1861986ca64d3f705bf7123178391255e89
Darcy Laycock <sutto@sutto.net>
2012-02-17 11:34:54 +0800

    Bump the version

--- lib/slugged/version.rb
+++ lib/slugged/version.rb
@@ -1,8 +1,8 @@
 module Slugged
   module Version
-    MAJOR  = 0
-    MINOR  = 4
-    PATCH  = 1
+    MAJOR  = 1
+    MINOR  = 0
+    PATCH  = 0
     STRING = [MAJOR, MINOR, PATCH].join(".")
   end
   
