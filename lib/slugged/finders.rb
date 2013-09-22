39ce7ec39b7771cca03e53836c39853ef5551548
Braulio Carreno <bcarreno@yahoo.com>
2013-09-21 22:48:36 -0400

    Relation#first with finder options is deprecated

--- lib/slugged/finders.rb
+++ lib/slugged/finders.rb
@@ -1,18 +1,18 @@
 module Slugged
   module Finders
     
-    def find_using_slug(slug, options = {})
+    def find_using_slug(slug)
       slug = slug.to_s
       value = nil
-      value ||= find_by_id(slug.to_i, options) if slug =~ /\A\d+\Z/
-      value ||= with_cached_slug(slug).first(options)
-      value ||= find_using_slug_history(slug, options) if use_slug_history
+      value ||= find_by_id(slug.to_i) if slug =~ /\A\d+\Z/
+      value ||= with_cached_slug(slug).first
+      value ||= find_using_slug_history(slug) if use_slug_history
       value.found_via_slug = slug if value.present?
       value
     end
     
-    def find_using_slug!(slug, options = {})
-      find_using_slug(slug, options) or raise ActiveRecord::RecordNotFound
+    def find_using_slug!(slug)
+      find_using_slug(slug) or raise ActiveRecord::RecordNotFound
     end
     
   end
--- test/helper.rb
+++ test/helper.rb
@@ -28,13 +28,13 @@ class Test::Unit::TestCase
     User.is_sluggable field, options
   end
   
-  def assert_same_as_slug(user, slug, options = {})
-    found_user = User.find_using_slug(slug, options)
+  def assert_same_as_slug(user, slug)
+    found_user = User.find_using_slug(slug)
     assert_equal user, found_user, "#{slug.inspect} should return #{user.inspect}, got #{found_user.inspect}"
   end
   
-  def assert_different_to_slug(user, slug, options = {})
-    found_user = User.find_using_slug(slug, options)
+  def assert_different_to_slug(user, slug)
+    found_user = User.find_using_slug(slug)
     assert_not_equal user, found_user, "#{slug.inspect} should not return #{user.inspect}, got same record."
   end
   
